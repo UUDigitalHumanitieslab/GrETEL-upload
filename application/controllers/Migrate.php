7da249194bc13ede63ffd816d429e1b15df2fc1d
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2016-11-22 10:28:25 +0100

    Added migrating to a specific version

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -18,13 +18,31 @@ class Migrate extends CI_Controller
 			return;
 		}
 
-		if ($this->migration->current() === FALSE)
+		if (!$this->migration->current())
 		{
 			show_error($this->migration->error_string());
 		}
 		else
 		{
-			echo 'Successfully ran migrations.' . PHP_EOL;
+			echo 'Successfully ran pending migrations.' . PHP_EOL;
+		}
+	}
+
+	public function version($id)
+	{
+		if (!is_cli())
+		{
+			echo 'This script can only be accessed via the command line.' . PHP_EOL;
+			return;
+		}
+
+		if (!$this->migration->version($id))
+		{
+			show_error($this->migration->error_string());
+		}
+		else
+		{
+			echo 'Successfully migrated to version ' . $id . '.' . PHP_EOL;
 		}
 	}
 }
