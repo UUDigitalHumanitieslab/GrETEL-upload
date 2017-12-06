944794a6a97b47bd3966d29fdac6637633ea32a6
Sheean Spoel <s.j.j.spoel@uu.nl>
2017-12-06 12:21:04 +0100

    Fixed unit tests

--- application/tests/controllers/Treebank_test.php
+++ application/tests/controllers/Treebank_test.php
@@ -2,23 +2,21 @@
 
 class Treebank_test extends TestCase
 {
+    public function test_index()
+    {
+        $output = $this->request('GET', '/treebank/');
+        $this->assertContains('<h2>Available treebanks</h2>', $output);
+    }
 
-	public function test_index()
-	{
-		$output = $this->request('GET', '/treebank/');
-		$this->assertContains('<h2>Available treebanks</h2>', $output);
-	}
+    public function test_show()
+    {
+        $this->request('GET', '/treebank/show/title');
+        $this->assertResponseCode(200);
+    }
 
-	public function test_show()
-	{
-		$this->request('GET', '/treebank/show/');
-		$this->assertResponseCode(404);
-	}
-
-	public function test_detail()
-	{
-		$this->request('GET', '/treebank/detail/');
-		$this->assertResponseCode(404);
-	}
-
+    public function test_detail()
+    {
+        $this->request('GET', '/treebank/detail/42');
+        $this->assertResponseCode(404);
+    }
 }
