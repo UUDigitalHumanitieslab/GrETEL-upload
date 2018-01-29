f89ec523836dd52480e0cc4658c23bf7f0f38b64
Sheean Spoel <s.j.j.spoel@uu.nl>
2018-01-29 17:46:54 +0100

    Check user permission when downloading a treebank. Resolves #5

--- application/controllers/api/Treebank.php
+++ application/controllers/api/Treebank.php
@@ -1,80 +1,88 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-require APPPATH . '/libraries/REST_Controller.php';
+require APPPATH.'/libraries/REST_Controller.php';
 
 class Treebank extends REST_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	public function __construct()
-	{
-		parent::__construct();
-	}
-	
-	/**
-	 * Downloads a big XML-file containing all the parsed tree.
-	 * TODO: limit access to public/owned treebanks
-	 */
-	public function download_get($title) {
-		set_time_limit(0);
-		$treebank = $this->treebank_model->get_treebank_by_title($title);
-		$this->basex->download(strtoupper($treebank->title . '_ID'));
-	}
+    /**
+     * Downloads a big XML-file containing all the parsed tree.
+     */
+    public function download_get($title)
+    {
+        set_time_limit(0);
+        $treebank = $this->treebank_model->get_treebank_by_title($title, current_user_id());
 
-	/**
-	 * Returns all public Treebanks (=public and processed)
-	 * @return JSON response.
-	 */
-	public function index_get()
-	{
-		$this->response($this->treebank_model->get_public_treebanks(current_user_id()));
-	}
+        if (!$treebank) {
+            $this->response(null, 403);
+        }
 
-	/**
-	 * Returns the Components of a Treebank, given its title.
-	 * @param  string $title The title of the Treebank.
-	 * @return JSON response.
-	 */
-	public function show_get($title)
-	{
-		$treebank = $this->treebank_model->get_treebank_by_title($title);
+        $this->basex->download(strtoupper($treebank->title.'_ID'));
+    }
 
-		if (!$treebank)
-		{
-			$this->response();
-		}
+    /**
+     * Returns all public Treebanks (=public and processed).
+     *
+     * @return JSON response
+     */
+    public function index_get()
+    {
+        $this->response($this->treebank_model->get_public_treebanks(current_user_id()));
+    }
 
-		$this->response($this->component_model->get_components_by_treebank($treebank->id));
-	}
+    /**
+     * Returns the Components of a Treebank, given its title.
+     *
+     * @param string $title the title of the Treebank
+     *
+     * @return JSON response
+     */
+    public function show_get($title)
+    {
+        $treebank = $this->treebank_model->get_treebank_by_title($title);
 
-	/**
-	 * Returns the Metadata of a Treebank, given its title.
-	 * @param  string $title The title of the Treebank.
-	 * @return JSON response.
-	 */
-	public function metadata_get($title)
-	{
-		$treebank = $this->treebank_model->get_treebank_by_title($title);
+        if (!$treebank) {
+            $this->response();
+        }
 
-		if (!$treebank)
-		{
-			$this->response();
-		}
+        $this->response($this->component_model->get_components_by_treebank($treebank->id));
+    }
 
-		$this->response($this->metadata_model->get_metadata_by_treebank($treebank->id, FALSE));
-	}
+    /**
+     * Returns the Metadata of a Treebank, given its title.
+     *
+     * @param string $title the title of the Treebank
+     *
+     * @return JSON response
+     */
+    public function metadata_get($title)
+    {
+        $treebank = $this->treebank_model->get_treebank_by_title($title);
 
-	/**
-	 * Returns all Treebanks for a User.
-	 * TODO: limit access to current User.
-	 * TODO: only return processed Treebanks in GrETEL.
-	 * @param  interger $user_id The ID of the User.
-	 * @return JSON response.
-	 */
-	public function user_get($user_id)
-	{
-		$this->response($this->treebank_model->get_treebanks_by_user($user_id));
-	}
+        if (!$treebank) {
+            $this->response();
+        }
 
+        $this->response($this->metadata_model->get_metadata_by_treebank($treebank->id, false));
+    }
+
+    /**
+     * Returns all Treebanks for a User.
+     * TODO: limit access to current User.
+     * TODO: only return processed Treebanks in GrETEL.
+     *
+     * @param interger $user_id the ID of the User
+     *
+     * @return JSON response
+     */
+    public function user_get($user_id)
+    {
+        $this->response($this->treebank_model->get_treebanks_by_user($user_id));
+    }
 }
--- application/models/Treebank_model.php
+++ application/models/Treebank_model.php
@@ -1,134 +1,161 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class Treebank_model extends CI_Model
 {
+    public function __construct()
+    {
+        parent::__construct();
 
-	public function __construct()
-	{
-		parent::__construct();
+        $this->load->database();
+    }
 
-		$this->load->database();
-	}
+    /**
+     * Retrieves all Treebanks.
+     *
+     * @return type
+     */
+    public function get_all_treebanks()
+    {
+        return $this->db->get('treebanks')->result();
+    }
 
-	/**
-	 * Retrieves all Treebanks.
-	 * @return type
-	 */
-	public function get_all_treebanks()
-	{
-		return $this->db->get('treebanks')->result();
-	}
+    /**
+     * Rertrieves Treebanks that still need to by processed.
+     *
+     * @return type
+     */
+    public function get_to_be_processed_treebanks()
+    {
+        $this->db->where('processed', null);
 
-	/**
-	 * Rertrieves Treebanks that still need to by processed.
-	 * @return type
-	 */
-	public function get_to_be_processed_treebanks()
-	{
-		$this->db->where('processed', NULL);
-		return $this->db->get('treebanks')->result();
-	}
+        return $this->db->get('treebanks')->result();
+    }
 
-	/**
-	 * Retrieves a Treebank model using its ID.
-	 * @param integer $treebank_id the ID of the Treebank
-	 * @return Treebank_model the found Treebank
-	 */
-	public function get_treebank_by_id($treebank_id)
-	{
-		$this->db->where('id', $treebank_id);
-		return $this->db->get('treebanks')->row();
-	}
-	
-	/**
-	 * Retrieves a Treebank model using its title.
-	 * @param string $title the title of the Treebank
-	 * @return Treebank_model the found Treebank
-	 */
-	public function get_treebank_by_title($title)
-	{
-		$this->db->where('title', $title);
-		return $this->db->get('treebanks')->row();
-	}
+    /**
+     * Retrieves a Treebank model using its ID.
+     *
+     * @param int $treebank_id the ID of the Treebank
+     *
+     * @return Treebank_model the found Treebank
+     */
+    public function get_treebank_by_id($treebank_id)
+    {
+        $this->db->where('id', $treebank_id);
 
-	/**
-	 * Creates a new Treebank model
-	 * @param array $treebank the fields for the Treebank
-	 * @return integer        the new ID for the Treebank
-	 */
-	public function add_treebank($treebank)
-	{
-		$this->db->insert('treebanks', $treebank);
-		return $this->db->insert_id();
-	}
+        return $this->db->get('treebanks')->row();
+    }
 
-	/**
-	 * Updates a Treebank model
-	 * @param integer $treebank_id the ID of the Treebank
-	 * @param array $treebank the fields for the Treebank
-	 */
-	public function update_treebank($treebank_id, $treebank)
-	{
-		$this->db->where('id', $treebank_id);
-		$this->db->update('treebanks', $treebank);
-	}
+    /**
+     * Retrieves a Treebank model using its title.
+     *
+     * @param string $title   the title of the Treebank
+     * @param int    $user_id the ID of the User to only get treebanks accessible for this user
+     *
+     * @return Treebank_model the found Treebank
+     */
+    public function get_treebank_by_title($title, $user_id = null)
+    {
+        $this->db->where('title', $title);
 
-	/**
-	 * Deletes a Treebank model, given its ID.
-	 * @param integer $treebank_id the ID of the Treebank
-	 */
-	public function delete_treebank($treebank_id)
-	{
-		$this->db->delete('treebanks', array('id' => $treebank_id));
-	}
+        if ($user_id) {
+            $this->db->group_start();
+            $this->db->where('public', true);
+            $this->db->or_where('user_id', $user_id);
+            $this->db->group_end();
+        }
 
-	/////////////////////////
-	// API Calls
-	/////////////////////////
+        return $this->db->get('treebanks')->row();
+    }
 
-	/**
-	 * Returns all Treebanks, including some information on the User that uploaded the Treebank.
-	 * @return array the found Treebanks.
-	 */
-	public function get_api_treebanks()
-	{
-		$this->db->select(array('treebanks.id', 'treebanks.title',
-			'users.id AS user_id', 'users.email',
-			'treebanks.uploaded', 'treebanks.processed', 'treebanks.public'));
-		$this->db->from('treebanks');
-		$this->db->join('users', 'users.id = treebanks.user_id');
-		return $this->db->get()->result();
-	}
+    /**
+     * Creates a new Treebank model.
+     *
+     * @param array $treebank the fields for the Treebank
+     *
+     * @return int the new ID for the Treebank
+     */
+    public function add_treebank($treebank)
+    {
+        $this->db->insert('treebanks', $treebank);
 
-	/**
-	 * Returns all processed Treebanks that are either public or uploaded by the given User.
-	 * @param integer $user_id the ID of the User
-	 * @return array the found Treebanks.
-	 */
-	public function get_public_treebanks($user_id = NULL)
-	{
-		$this->db->where('processed IS NOT NULL');
-		$this->db->group_start();
-		$this->db->where('public', TRUE);
-		if ($user_id)
-		{
-			$this->db->or_where('user_id', $user_id);
-		}	
-		$this->db->group_end();
-		return $this->get_api_treebanks();
-	}
+        return $this->db->insert_id();
+    }
 
-	/**
-	 * Returns all Treebanks uploaded by the given User.
-	 * @param integer $user_id the ID of the User
-	 * @return array the found Treebanks.
-	 */
-	public function get_treebanks_by_user($user_id)
-	{
-		$this->db->where('user_id', $user_id);
-		return $this->get_api_treebanks();
-	}
+    /**
+     * Updates a Treebank model.
+     *
+     * @param int   $treebank_id the ID of the Treebank
+     * @param array $treebank    the fields for the Treebank
+     */
+    public function update_treebank($treebank_id, $treebank)
+    {
+        $this->db->where('id', $treebank_id);
+        $this->db->update('treebanks', $treebank);
+    }
 
+    /**
+     * Deletes a Treebank model, given its ID.
+     *
+     * @param int $treebank_id the ID of the Treebank
+     */
+    public function delete_treebank($treebank_id)
+    {
+        $this->db->delete('treebanks', array('id' => $treebank_id));
+    }
+
+    /////////////////////////
+    // API Calls
+    /////////////////////////
+
+    /**
+     * Returns all Treebanks, including some information on the User that uploaded the Treebank.
+     *
+     * @return array the found Treebanks
+     */
+    public function get_api_treebanks()
+    {
+        $this->db->select(array('treebanks.id', 'treebanks.title',
+            'users.id AS user_id', 'users.email',
+            'treebanks.uploaded', 'treebanks.processed', 'treebanks.public', ));
+        $this->db->from('treebanks');
+        $this->db->join('users', 'users.id = treebanks.user_id');
+
+        return $this->db->get()->result();
+    }
+
+    /**
+     * Returns all processed Treebanks that are either public or uploaded by the given User.
+     *
+     * @param int $user_id the ID of the User
+     *
+     * @return array the found Treebanks
+     */
+    public function get_public_treebanks($user_id = null)
+    {
+        $this->db->where('processed IS NOT NULL');
+        $this->db->group_start();
+        $this->db->where('public', true);
+        if ($user_id) {
+            $this->db->or_where('user_id', $user_id);
+        }
+        $this->db->group_end();
+
+        return $this->get_api_treebanks();
+    }
+
+    /**
+     * Returns all Treebanks uploaded by the given User.
+     *
+     * @param int $user_id the ID of the User
+     *
+     * @return array the found Treebanks
+     */
+    public function get_treebanks_by_user($user_id)
+    {
+        $this->db->where('user_id', $user_id);
+
+        return $this->get_api_treebanks();
+    }
 }
