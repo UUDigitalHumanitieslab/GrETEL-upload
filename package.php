a481b6717f3443b32436955c703fcd5076bb9ca6
Sheean Spoel <s.j.j.spoel@uu.nl>
2018-03-07 09:45:38 +0100

    Version 0.2

--- package.php
+++ package.php
@@ -1,3 +1,3 @@
-<?php // Generated on Wed Mar 07 2018 09:41:46 GMT+0100 (CET)
-define('VERSION', '0.1.142');
+<?php // Generated on Wed Mar 07 2018 09:44:53 GMT+0100 (CET)
+define('VERSION', '0.2.0');
 define('DATE', 'Wed Mar 07 2018');
