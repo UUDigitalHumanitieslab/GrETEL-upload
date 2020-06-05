261a2e59a9c063d50718e9501c01a8edab497ba8
Sheean Spoel <s.j.j.spoel@uu.nl>
2020-06-05 14:20:44 +0200

    pure-min.css isn't served over https

--- application/views/header.php
+++ application/views/header.php
@@ -8,7 +8,7 @@
 <script src="//code.jquery.com/jquery-3.1.1.min.js"></script>
 <script src="//cdn.jsdelivr.net/qtip2/3.0.3/jquery.qtip.min.js"></script>
 <?=link_tag('//cdn.jsdelivr.net/qtip2/3.0.3/jquery.qtip.min.css'); ?>
-<?=link_tag('//yui.yahooapis.com/pure/0.6.0/pure-min.css'); ?>
+<?=link_tag('css/pure-min.css'); ?>
 <?=link_tag('//fonts.googleapis.com/css?family=Oswald:400,300,700|Carrois+Gothic'); ?>
 <?=link_tag('css/style.css'); ?>
 
@@ -17,4 +17,4 @@
 	<div id="wrapper" class="pure-u-3-4">
 		<img id="header-img" src="images/uu-header.png">
 		<?=heading(lang('site_title'), 1); ?>
-		<?=$this->load->view('menu', NULL, TRUE); ?>
+		<?=$this->load->view('menu', null, true); ?>
