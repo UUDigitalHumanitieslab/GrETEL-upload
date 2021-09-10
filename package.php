eccd14d5425a39f48436bab241fdc4debf946b49
Sheean Spoel <sheean@sheean.nl>
2021-09-10 13:42:57 +0200

    0.2.3

--- package.php
+++ package.php
@@ -1,3 +1,3 @@
-<?php // Generated on Fri Jun 05 2020 10:37:08 GMT+0200 (Central European Summer Time)
-define('VERSION', '0.2.1');
-define('DATE', 'Fri Jun 05 2020');
+<?php // Generated on Fri Sep 10 2021 13:42:22 GMT+0200 (Central European Summer Time)
+define('VERSION', '0.2.3');
+define('DATE', 'Fri Sep 10 2021');
