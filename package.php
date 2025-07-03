01ef35c9a547941883486cdf500db6f9d758a666
Sheean Spoel <s.j.j.spoel@uu.nl>
2025-07-03 11:46:27 +0200

    0.2.4

--- package.php
+++ package.php
@@ -1,3 +1,3 @@
-<?php // Generated on Fri Sep 10 2021 13:42:22 GMT+0200 (Central European Summer Time)
-define('VERSION', '0.2.3');
-define('DATE', 'Fri Sep 10 2021');
+<?php // Generated on Thu Jul 03 2025 11:46:17 GMT+0200 (Central European Summer Time)
+define('VERSION', '0.2.4');
+define('DATE', 'Thu Jul 03 2025');
